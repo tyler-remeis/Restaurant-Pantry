1f523a8512d63009312ef1f4f2f9358120702243
tyler_remeis <tyler.remeis@gmail.com>
2019-09-15 19:49:13 -0400

    changed file directory for main GET route

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('public')); /* this line tells Express to use the public folder as our static folder from which we can serve static files*/
+app.use(express.static('C:\Users\Tyler Remeis\Desktop\Microsoft DEV Courses\Microsoft DEV284x\PantryApp')); /* this line tells Express to use the public folder as our static folder from which we can serve static files*/
 
 app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
