b1c0562f8baec230cc770da494af05b7d848b8bc
tyler_remeis <tyler.remeis@gmail.com>
2019-09-15 19:59:45 -0400

    second attempt with sendFile()

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var app = express();
-
-app.use(express.static('C:\Users\Tyler Remeis\Desktop\Microsoft DEV Courses\Microsoft DEV284x\PantryApp')); /* this line tells Express to use the public folder as our static folder from which we can serve static files*/
+var path = require('path')
 
 app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
 
 app.get('/', function(req, res){
-    res.sendFile('Login.html');
+    res.sendFile(path.join(C:\Users\Tyler Remeis\Desktop\Microsoft DEV Courses\Microsoft DEV284x\PantryApp + '/Login.html'));
 }); 
