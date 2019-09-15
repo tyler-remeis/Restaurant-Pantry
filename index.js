7a46fc18cd2f06a7a98c9e0b0370becf1b27ebc4
tyler_remeis <tyler.remeis@gmail.com>
2019-09-15 19:46:17 -0400

    testing adding GET path

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('public')); /* this line tells Express to use the public folder as our static folder from which we can serve static files*/
+
 app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
+
+app.get('/', function(req, res){
+    res.sendFile('Login.html');
+}); 
