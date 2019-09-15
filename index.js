620ea3cb18d15df9fcaaf9d9af1628844525ce1f
tyler_remeis <tyler.remeis@gmail.com>
2019-09-15 18:14:47 -0400

    changed GET path on index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello world!');
-});
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
