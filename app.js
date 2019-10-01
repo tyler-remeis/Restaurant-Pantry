0c78ceae33230348f12c301f2dba8df125a6d4e0
tyler_remeis <tyler.remeis@gmail.com>
2019-10-01 06:58:57 -0400

    fixing listening port

--- app.js
+++ app.js
@@ -41,18 +41,17 @@ app.use(session({
 
 require('./app/server/routes')(app);
 
+/*
 http.createServer(app).listen(app.get('port'), function(){
 	console.log('Express server listening on port ' + app.get('port'));
 });
+*/
 
-
-
-
-/*
 app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
 
+/*
 //Connecting style sheet
 app.use(express.static(path.join(__dirname +'/public'))); 
 
