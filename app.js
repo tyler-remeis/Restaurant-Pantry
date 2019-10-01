7a9861ca61f08a278b48fc035cddd687be16b450
tyler_remeis <tyler.remeis@gmail.com>
2019-10-01 11:08:41 -0400

    db env vars

--- app.js
+++ app.js
@@ -19,9 +19,9 @@ app.use(express.static(__dirname + '/app/public'));
 
 // build mongo database connection url //
 
-process.env.DB_HOST = process.env.DB_HOST || 'localhost'
-process.env.DB_PORT = process.env.DB_PORT || 27017;
-process.env.DB_NAME = process.env.DB_NAME || 'node-login';
+process.env.DB_HOST = process.env.DB_HOST || 'ds135486.mlab.com'
+process.env.DB_PORT = process.env.DB_PORT || 35486;
+process.env.DB_NAME = process.env.DB_NAME || 'heroku_pb7h12dm';
 
 if (app.get('env') != 'live'){
 	process.env.DB_URL = 'mongodb://'+process.env.DB_HOST+':'+process.env.DB_PORT;
