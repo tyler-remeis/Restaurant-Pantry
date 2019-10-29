15bdf620b060351b42e6738e53d182646e2755d9
tyler_remeis <tyler.remeis@gmail.com>
2019-10-29 06:44:16 -0400

    new db url

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ process.env.DB_NAME = process.env.DB_NAME || 'heroku_pb7h12dm';
 
 
 if (app.get('env') != 'live'){
-	process.env.DB_URL = 'mongodb://'+process.env.DB_HOST+':'+process.env.DB_PORT+'/'+process.env.DB_NAME;
+	process.env.DB_URL = 'mongodb://'+process.env.DB_USER+':'+process.env.DB_PASS+'@'+process.env.DB_HOST+':'+process.env.DB_PORT+'/'+process.env.DB_NAME;
 	console.log(`line 29`)
 }	else {
 // prepend url with authentication credentials // 
